b360afaa3568061c2e2a6217ae83c9ece3f6abf2
Mateo-mh <byronmh8@gmail.com>
2023-06-23 13:15:13 -0500

    DB creada con exito

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import store from './redux/store.js';
 import { Provider } from 'react-redux';
 
 
+
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,8 @@
-const express = require('express');
-// const server = express();
-const server = require('./app.js'); 
-const PORT = 3001
+const server = require('./app.js');
+const { conn } = require('./DB_connection.js'); // Importa el objeto Sequelize completo
+const PORT = 3001;
 
-server.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
+conn.sync({force: true}).then(() => {
+  server.listen(PORT, () => console.log(`Listening on port: ${PORT}`)); 
+})
+
